85ba2ee3fb77d5304aed60c3fec704e399fb71a2
James Duffy <jamesmartinduffy@gmail.com>
2014-05-29 17:00:27 +0900

    Added _decorator line to engine.rb

--- lib/spree_korean_surface_mail_calculator/engine.rb
+++ lib/spree_korean_surface_mail_calculator/engine.rb
@@ -9,5 +9,11 @@ module SpreeKoreanSurfaceMailCalculator
       require 'spree/calculator/shipping/korean_surface_mail'
       app.config.spree.calculators.tax_rates << Spree::Calculator::KoreanSurfaceMail
     end
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
   end
 end
