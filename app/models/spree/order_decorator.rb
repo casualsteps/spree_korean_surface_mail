bd6ad79c21eac19b951ef7996438e81c349162e2
James Duffy <jamesmartinduffy@gmail.com>
2014-09-04 18:27:56 +0900

    Changed display methods to output HTML (for currency styling)

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,12 +2,12 @@ Spree::Order.class_eval do
   def display_presentation_gwansae
     @rate = @rate || Spree::CurrencyRate.find_by(:target_currency => 'KRW')
     in_won = @rate.convert_to_won(gwansae).to_f
-    Spree::Money.new(in_won, {currency: "KRW"})
+    Spree::Money.new(in_won, {currency: "KRW"}).to_html
   end
 
   def display_presentation_bugasae
     @rate = @rate || Spree::CurrencyRate.find_by(:target_currency => 'KRW')
     in_won = @rate.convert_to_won(bugasae).to_f
-    Spree::Money.new(in_won, {currency: "KRW"})
+    Spree::Money.new(in_won, {currency: "KRW"}).to_html
   end
 end
